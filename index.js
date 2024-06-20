f52c589ec2f2735a7c6b100b4a5d0673c85f30a9
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-20 10:48:47 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Subira', 
+    streetAddress: 'South C',
+}
+
+// Function to update an employee with a new key-value pair non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+    console.log('*************************************');
+    console.log('      Update Non-Destructively       ');
+    console.log('*************************************');
+
+    const newEmployee = { ...employee };
+    console.log(`Before Update: ${newEmployee.name}`);
+    newEmployee[key] = value;
+    console.log(`After Update: ${newEmployee.name}`);
+    return newEmployee;
+}
+  
+// Function to update an employee with a new key-value pair destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    console.log('*************************************');
+    console.log('        Update Destructively         ');
+    console.log('*************************************');
+
+    console.log(`Before Update: ${employee.name}`);
+    employee[key] = value;
+    console.log(`After Update: ${employee.name}`);
+    return employee;
+}
+  
+// Function to delete a key from an employee object non-destructively
+function deleteFromEmployeeByKey(employee, key) {
+
+    console.log('*************************************');
+    console.log('        Delete by Employee Key       ');
+    console.log('*************************************');
+
+    let newEmployee = {};
+    console.log(`Before Declaration: ${newEmployee.name}`);
+    newEmployee = { ...employee };
+    console.log(`Before Delete: ${newEmployee.name}`);
+    delete newEmployee[key];
+    console.log(`Before Update: ${newEmployee.name}`);
+    return newEmployee;
+}
+  
+// Function to delete a key from an employee object destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    console.log('**************************************');
+    console.log(' Delete destructively by Employee Key ');
+    console.log('**************************************');
+
+    console.log(`Before Delete: ${employee.name}`);
+    delete employee[key];
+    console.log(`After Delete: ${employee.name}`)
+    return employee;
+}
